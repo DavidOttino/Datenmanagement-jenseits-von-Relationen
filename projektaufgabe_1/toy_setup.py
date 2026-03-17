9736af856dbc17264027982a2e5f15538ca4422d
David Ottino <david.ottino@stud.plus.ac.at>
2026-03-17 23:46:41 +0100

    Implement generator

--- projektaufgabe_1/toy_setup.py
+++ projektaufgabe_1/toy_setup.py
@@ -1,5 +1,7 @@
 import os
 import psycopg
+import random
+import string
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -120,5 +122,86 @@ def toy_setup():
         print(f"Error: {e}")
     print("[SETUP] Completed toy_setup!")
 
+def generate(num_tuples: int, sparsity: float, num_attributes: int):
+    print("[GENERATE] Starting data generation...")
+    
+    # most possible attributes in Postgres is 1.600
+    if num_attributes > 1599:
+        print("[GENERATE] Unable to generate H with more than 1.600 attributes!")
+        return
+    
+    try:
+        with psycopg.connect(get_conn_str()) as conn:
+            with conn.cursor() as cur:
+                # delete old data
+                cur.execute("DROP TABLE IF EXISTS H CASCADE;")
+
+                # generate attributes
+                columns = ["oid INTEGER"]
+                for i in range(num_attributes):
+                    if i % 2 == 0:
+                        columns.append(f"attr{i+1} INTEGER")
+                    else:
+                        columns.append(f"attr{i+1} TEXT")
+                columns_sql = ", ".join(columns)
+
+                cur.execute(f"CREATE TABLE H ({columns_sql});")
+
+                # generate value pools
+                attr_values = []
+                for i in range(num_attributes):
+                    pool = []
+                    max_unique_values = (num_tuples // 5) + 1
+                    # Ints
+                    if i % 2 == 0:
+                        values = list(range(1, max_unique_values + 1))
+                        for v in values:
+                            pool.extend([v]*5)
+                    # Strings
+                    else:
+                        values = [''.join(random.choices(string.ascii_uppercase, k=3)) for _ in range(max_unique_values)]
+                        for v in values:
+                            pool.extend([v]*5)
+                    random.shuffle(pool)
+                    attr_values.append(pool[:num_tuples])
+
+                # insert values from pools, add null values according to sparsity
+                for t in range(num_tuples):
+                    row = [str(t)]
+                    for a in range(num_attributes):
+                        if random.random() < sparsity:
+                            row.append('NULL')
+                        else:
+                            val = attr_values[a].pop()
+                            if isinstance(val, str):
+                                row.append(f"'{val}'")
+                            else:
+                                row.append(str(val))
+                    row_sql = ", ".join(row)
+                    cur.execute(f"INSERT INTO H VALUES ({row_sql});")
+
+                # create views for checking the data
+                cur.execute("""
+                CREATE OR REPLACE VIEW view_sparsity AS
+                select
+                    (select count(*) from H where attr1 is NULL)::numeric / count(*) as sparsity
+                from H;
+                """)
+
+                cur.execute("""
+                CREATE OR REPLACE VIEW view_max_attr1 AS 
+                select attr1, count(*) AS count_per_value
+                from H
+                where attr1 is not null
+                group by attr1
+                having count(*) > 5;
+                """)
+
+        print(f"[GENERATE] Completed data generation: {num_tuples} tuples with {num_attributes} attributes.")
+
+    except Exception as e:
+        print(f"Error: {e}")
+
 if __name__ == "__main__":
     toy_setup()
+    generate(10000, 0.2, 100)
