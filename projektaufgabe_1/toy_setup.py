d1412aa872b7ca351fe71f0a6db7e6e3a5ff49d6
Christian Zanotti <christian.zanotti@tum.de>
2026-03-17 17:28:39 +0100

    Implemented Phase 1 point a: Automatic H_toy table

--- projektaufgabe_1/toy_setup.py
+++ projektaufgabe_1/toy_setup.py
@@ -17,7 +17,20 @@ def toy_setup():
     try:
         with psycopg.connect(get_conn_str()) as conn:
             with conn.cursor() as cur:
-                #TODO
+                cur.execute("DROP TABLE IF EXISTS H_toy CASCADE;")
+                cur.execute("CREATE TABLE H_toy (oid INT, a1 TEXT, a2 TEXT, a3 INT);")
+                print("Created table H_toy")
+
+                data_h = [
+                    (1, 'a', 'b', None),
+                    (2, None, 'c', 2),
+                    (3, None, None, 3),
+                    (4, None, None, None)
+                ]
+
+                cur.executemany("INSERT INTO H_toy VALUES (%s, %s, %s, %s)", data_h)
+                print("Populated table H_toy")
+
     except Exception as e:
         print(f"Error: {e}")
 
