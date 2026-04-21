2020b64ff3ccbe3ba51724ac444d40da3612af1a
Martin Wiesinger <martinwiesinger@pop-os.localdomain>
2026-04-21 23:03:35 +0200

    fix issues with benchmark (to drop the old function if a different attribute count is selected)

--- projektaufgabe_1/queryAPI.py
+++ projektaufgabe_1/queryAPI.py
@@ -19,8 +19,8 @@ class queryAPI:
         for a in attr_list:
             part = f"""
             COALESCE(
-                (SELECT val::TEXT FROM {v_int} WHERE oid = p_oid AND key = '{a}'),
-                (SELECT val FROM {v_text} WHERE oid = p_oid AND key = '{a}')
+                (SELECT vi.val::TEXT FROM {v_int} vi WHERE vi.oid = p_oid AND vi.key = '{a}'),
+                (SELECT vt.val FROM {v_text} vt WHERE vt.oid = p_oid AND vt.key = '{a}')
             )"""
             select_parts.append(part)
 
@@ -37,17 +37,23 @@ class queryAPI:
         """
 
         q_ii_sql = f"""
-        CREATE OR REPLACE FUNCTION q_ii(p_key TEXT, p_val INT)
+        CREATE OR REPLACE FUNCTION q_ii(p_key TEXT, p_val TEXT)
         RETURNS TABLE (oid INT) AS $$
         BEGIN
             RETURN QUERY
             SELECT v.oid FROM {v_int} v
+            WHERE v.key = p_key AND v.val::TEXT = p_val
+            UNION ALL
+            SELECT v.oid FROM {v_text} v
             WHERE v.key = p_key AND v.val = p_val;
         END;
         $$ LANGUAGE plpgsql;
         """
 
         with self.conn.cursor() as cur:
+            cur.execute("DROP FUNCTION IF EXISTS q_i(INT);")
+            cur.execute("DROP FUNCTION IF EXISTS q_ii(TEXT, INT);")
+            cur.execute("DROP FUNCTION IF EXISTS q_ii(TEXT, TEXT);")
             cur.execute(q_i_sql)
             cur.execute(q_ii_sql)
         self.conn.commit()
@@ -58,9 +64,9 @@ class queryAPI:
             cur.execute("SELECT * FROM q_i(%s);", (oid,))
             return cur.fetchall()
 
-    def q_ii(self, key: str, val: int):
+    def q_ii(self, key: str, val):
         with self.conn.cursor() as cur:
-            cur.execute("SELECT * FROM q_ii(%s, %s);", (key, val))
+            cur.execute("SELECT * FROM q_ii(%s, %s);", (key, str(val)))
             return cur.fetchall()
         
     def explain_q_i(self, oid: int):
@@ -68,7 +74,7 @@ class queryAPI:
             cur.execute("EXPLAIN ANALYZE SELECT * FROM q_i(%s);", (oid,))
             return [row[0] for row in cur.fetchall()]
 
-    def explain_q_ii(self, key: str, val: int):
+    def explain_q_ii(self, key: str, val):
         with self.conn.cursor() as cur:
-            cur.execute("EXPLAIN ANALYZE SELECT * FROM q_ii(%s, %s);", (key, val))
+            cur.execute("EXPLAIN ANALYZE SELECT * FROM q_ii(%s, %s);", (key, str(val)))
             return [row[0] for row in cur.fetchall()]
